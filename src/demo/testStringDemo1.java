6c1d736932c923835c6e263c3ebb85cd321e3c19
a616285195 <616285195@qq.com>
2018-06-04 19:50:18 +0800

    first commit

---
+++ src/demo/testStringDemo1.java
@@ -0,0 +1,28 @@
+package demo;
+
+import demo.StringDemo;
+import junit.framework.TestCase;
+public class testStringDemo1 extends TestCase {
+	private StringDemo str;
+	protected void setUp(){
+		str = new StringDemo();
+	}
+		public void testSmallString() {
+		assertEquals("一个字母变小写",str.smallString("A"),"a");
+		assertEquals("一个字母变小写",str.smallString("B"),"b");
+		assertEquals("一个字母变小写",str.smallString("C"),"c");
+		assertEquals("一个字母变小写",str.smallString("D"),"d");
+		assertEquals("一个字母变小写",str.smallString("E"),"e");
+		assertEquals("一个字母变小写",str.smallString("F"),"f");
+		assertEquals("一个字母变小写",str.smallString("G"),"g");
+		assertEquals("一个字母变小写",str.smallString("H"),"h");
+		assertEquals("一个字母变小写",str.smallString("I"),"i");
+		assertEquals("一个字母变小写",str.smallString("J"),"j");
+		assertEquals("一个字母变小写",str.smallString("K"),"k");
+		assertEquals("一个字母变小写",str.smallString("L"),"l");
+		assertEquals("一个字母变小写",str.smallString("M"),"m");
+
+
+		}
+		
+	}
